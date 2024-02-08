fb3cba77c16a7bcfc94cd575267cece001aa49b0
Vloukk <v.valentin.evangelista@gmail.com>
2024-02-08 14:21:21 +0100

    numerotation carousel

--- src/components/Layout/Carousel.js
+++ src/components/Layout/Carousel.js
@@ -10,14 +10,28 @@ const Carousel = ({ images }) => {
     const prevSlide = () => {
       setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
     };
-  
+
+    // Condition pour masquer les flèches et la numérotation si le carrousel contient une seule image
+    const hideControls = images.length <= 1;
+
     return (
       <div className="carousel">
-        <span className="prev" onClick={prevSlide}><i className="fa-solid fa-chevron-left"></i></span>
+        {/* Condition pour afficher la flèche de gauche */}
+        {!hideControls && (
+          <span className="prev" onClick={prevSlide}><i className="fa-solid fa-chevron-left"></i></span>
+        )}
         <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
-        <span className="next" onClick={nextSlide}><i className="fa-solid fa-chevron-right"></i></span>
+        {/* Condition pour afficher la flèche de droite */}
+        {!hideControls && (
+          <span className="next" onClick={nextSlide}><i className="fa-solid fa-chevron-right"></i></span>
+        )}
+        {/* Condition pour afficher la numérotation */}
+        {!hideControls && (
+          <div className="carousel__counter">{currentIndex + 1} / {images.length}</div>
+        )}
       </div>
     );
   };
   
   export default Carousel;
+
